3eefbd3469a254eff157d13a2fce9cedd4f3e7e5
unknown <mugdhazope31@gmail.com>
2024-11-28 11:23:10 +0530

    Committed

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        MONGODB_URI: process.env.MONGODB_URI,
+        GITHUB_ID: process.env.GITHUB_ID,
+        GITHUB_SECRET: process.env.GITHUB_SECRET
+    }
 };
 
 export default nextConfig;
